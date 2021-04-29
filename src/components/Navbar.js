6e9a3ec610eca709cc24ffb4b2812244f8c2e44e
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2021-04-29 16:01:38 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
       {isUser && user.picture && <img src={user.picture} alt={user.name} />}
       {isUser && user.name && (
         <h4>
-          Welcome, <strong>{user.name}</strong>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
         </h4>
       )}
       {isUser ? (
